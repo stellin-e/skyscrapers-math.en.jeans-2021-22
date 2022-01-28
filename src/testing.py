a7b0495a0f4b3338ad6f8d0496021c89f07f219a
sdaft <80103996+stellin-e@users.noreply.github.com>
2022-01-28 16:28:06 +0100

    Create testing.py

---
+++ src/testing.py
@@ -0,0 +1,13 @@
+from timeit import timeit
+
+code = """
+from city import City
+
+city = City([3, 2, 1, 3], [3, 2, 1, 3], [2, 3, 2, 1], [2, 3, 2, 1])
+city.solve_1st()
+"""
+
+print(f"""
+solve_1st: {timeit(code, number=1)}
+
+""")
