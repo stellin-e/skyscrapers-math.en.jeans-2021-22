efaf0405f2178e2a380b5997468325959dfb2f89
sdaft <80103996+stellin-e@users.noreply.github.com>
2022-01-29 15:43:18 +0100

    Update testing.py

--- src/testing.py
+++ src/testing.py
@@ -3,11 +3,12 @@ from timeit import timeit
 code = """
 from city import City
 
+
 city = City([3, 2, 1, 3], [3, 2, 1, 3], [2, 3, 2, 1], [2, 3, 2, 1])
 city.solve_1st()
 """
 
 print(f"""
-solve_1st: {timeit(code, number=1)}
+solve_1st: {timeit(code, number=1):.5f}
 
 """)
