395dc5b9f84b317bc4e7c94c483b3a93eb18d71c
stellin-e <80103996+stellin-e@users.noreply.github.com>
2022-03-31 00:17:30 +0200

    Started work on non-unique city algorithm

--- src/find_all_cities.py
+++ src/find_all_cities.py
@@ -1,4 +1,5 @@
 from datetime import datetime, timezone
+from re import I
 from const import *
 import numpy as np
 from pprint import pprint
@@ -79,6 +80,116 @@ def solve_city(top, left, bottom, right):
     
     return rows
 
+def solve_non_unique(rows, top, left, bottom, right):
+    solutions = []
+    try:
+
+        is_unique = True
+        pos_of_sets = []
+
+        for y in range(4):
+            for x in range(4):
+                c = rows[y][x]
+
+                if type(c) == set:
+                    is_unique = False
+                    pos_of_sets.append((x, y))
+
+        pos_first_set = pos_of_sets[0]
+        print(rows)
+        first_row = rows[pos_first_set[1]][pos_first_set[0]]
+        for v in list(first_row):
+            rows[pos_first_set[1]][pos_first_set[0]] = v
+            
+            # Calculate rows
+            # TODO: FIX ALGO
+            for y in range(4):
+                col = rows[y]
+                known_values = set()
+                unknown_value = None
+
+                for cell in col:
+                    if type(cell) == int:
+                        known_values.update({cell})
+                    else:
+                        unknown_value = cell
+                
+                if len(known_values) == 3:
+                    unknown_value_index = col.index(unknown_value)
+                    rows[y][unknown_value_index] = unknown_value - known_values
+            
+            for x in range(4):
+                col = rows[x]
+                known_values = set()
+                unknown_value = None
+
+                for cell in col:
+                    if type(cell) == int:
+                        known_values.update({cell})
+                    else:
+                        unknown_value = cell
+                
+                if len(known_values) == 3:
+                    unknown_value_index = col.index(unknown_value)
+                    rows[y][unknown_value_index] = unknown_value - known_values
+                        
+                    
+
+
+
+            # # Subtract columns
+            # for y in range(4):
+            #     row = rows[y]
+            #     known_values = set() # Set with the known values of the row
+            #     unknown_value = None
+
+            #     for c in row:
+                    
+            #         if len(c) == 1 or type(c) == int:
+            #             # NOTE: .update() updates the set with the union of itself and cell
+            #             known_values.update(c)
+            #         else:
+            #             unknown_value = c
+                
+            #     if len(known_values) == 3:
+            #         # Find the unknown value in the row and subtract its value
+            #         # with the known values
+            #         unknown_value_index = row.index(unknown_value)
+            #         rows[y][unknown_value_index] = unknown_value - known_values
+
+            # for x in range(4):
+                
+            #     col = [rows[0][x], rows[1][x], rows[2][x], rows[3][x]]
+
+            #     known_values = set() # Set with the known values of the column
+            #     unknown_value = None
+
+            #     for c in col:
+                    
+            #         if len(c) == 1 or type(c) == int:
+            #             # NOTE: .update() updates the set with the union of itself and cell
+            #             known_values.update(c)
+            #         else:
+            #             unknown_value = c
+                
+            #     if len(known_values) == 3:
+            #         # Find the unknown value in the column and subtract its value
+            #         # with the known values
+            #         unknown_value_index = col.index(unknown_value)
+            #         rows[unknown_value_index][x] = unknown_value - known_values
+
+            print(rows)
+
+
+        # If the city is unique, return the city
+        if is_unique:
+            return rows
+    
+    except Exception as e:
+        raise e
+
+    return solutions
+
 def check_solution(top, left, bottom, right, rows: list, return_formatted=False):
     """
         Check if a solution given in rows is valid.
@@ -116,8 +227,6 @@ def check_solution(top, left, bottom, right, rows: list, return_formatted=False)
     
     return valid
 
-
-
 def generalize_row(row):
     possible_exts = []
 
@@ -135,93 +244,100 @@ def generalize_row(row):
 
     return possible_exts
 
+x = solve_city([1, 2, 2, 4], [1, 2, 2, 4], [4, 2, 2, 1], [4, 2, 2, 1])
+solve_non_unique(x, [1, 2, 2, 4], [1, 2, 2, 4], [4, 2, 2, 1], [4, 2, 2, 1])
     
-extremes = [
-    (2, 2),
-    (1, 4),
-    (2, 1),
-    (1, 2),
-    (1, 3),
-    (2, 3),
-    (3, 1),
-    (4, 1),
-    (3, 2)
-]
+# extremes = [
+#     (2, 2),
 
-invalids = []
-valids = []
-print(
-    generalize_row(
-        ((1, 2, 4,), (3,), (1, 2), (1, 2, 4))
-    )
-)
+#     (1, 4),
+#     (4, 1),
+
+#     (2, 1),
+#     (1, 2),
+
+#     (1, 3),
+#     (3, 1),
+
+#     (2, 3),
+#     (3, 2)
+# ]
+
+# invalids = []
+# valids = []
+# print(
+#     generalize_row(
+#         ((1, 2, 4,), (3,), (1, 2), (1, 2, 4))
+#     )
+# )
 
 
-for col1 in extremes:
-    for col2 in extremes:
-        for col3 in extremes:
-            for col4 in extremes:
-                columns = [col1, col2, col3, col4]
-                # Get top and bottom extremes
-                top = [i[0] for i in columns]
-                bottom = [i[1] for i in columns]
+# for col1 in extremes:
+#     for col2 in extremes:
+#         for col3 in extremes:
+#             for col4 in extremes:
+#                 columns = [col1, col2, col3, col4]
+#                 # Get top and bottom extremes
+#                 top = [i[0] for i in columns]
+#                 bottom = [i[1] for i in columns]
 
-                inner_rows = [
-                    [], 
-                    [], 
-                    [], 
-                    []
-                ]
+#                 inner_rows = [
+#                     [], 
+#                     [], 
+#                     [], 
+#                     []
+#                 ]
 
-                inner0 = GEN_CONF_TUPLES[(top[0], bottom[0])]
-                inner1 = GEN_CONF_TUPLES[(top[1], bottom[1])]
-                inner2 = GEN_CONF_TUPLES[(top[2], bottom[2])]
-                inner3 = GEN_CONF_TUPLES[(top[3], bottom[3])]
+#                 inner0 = GEN_CONF_TUPLES[(top[0], bottom[0])]
+#                 inner1 = GEN_CONF_TUPLES[(top[1], bottom[1])]
+#                 inner2 = GEN_CONF_TUPLES[(top[2], bottom[2])]
+#                 inner3 = GEN_CONF_TUPLES[(top[3], bottom[3])]
 
-                inners = [inner0, inner1, inner2, inner3]
+#                 inners = [inner0, inner1, inner2, inner3]
 
-                for inner in inners:
-                    for i, number in zip(range(len(inner_rows)), inner):
-                        inner_rows[i].append(number)
+#                 for inner in inners:
+#                     for i, number in zip(range(len(inner_rows)), inner):
+#                         inner_rows[i].append(number)
 
-                try:
-                    # Calculate left and right extremes
-                    left = []
-                    right = []
+#                 try:
+#                     # Calculate left and right extremes
+#                     left = []
+#                     right = []
 
-                    pos_ext0 = generalize_row(inner_rows[0])
-                    pos_ext1 = generalize_row(inner_rows[1])
-                    pos_ext2 = generalize_row(inner_rows[2])
-                    pos_ext3 = generalize_row(inner_rows[3])
+#                     pos_ext0 = generalize_row(inner_rows[0])
+#                     pos_ext1 = generalize_row(inner_rows[1])
+#                     pos_ext2 = generalize_row(inner_rows[2])
+#                     pos_ext3 = generalize_row(inner_rows[3])
                         
-                    for ext0 in pos_ext0:
-                        for ext1 in pos_ext1:
-                            for ext2 in pos_ext2:
-                                for ext3 in pos_ext3:
-                                    left = [ext0[0], ext1[0], ext2[0], ext3[0]]
-                                    right = [ext0[1], ext1[1], ext2[1], ext3[1]]
+#                     for ext0 in pos_ext0:
+#                         for ext1 in pos_ext1:
+#                             for ext2 in pos_ext2:
+#                                 for ext3 in pos_ext3:
+#                                     left = [ext0[0], ext1[0], ext2[0], ext3[0]]
+#                                     right = [ext0[1], ext1[1], ext2[1], ext3[1]]
                                     
-                                    borders = [top, left, bottom, right]
-                                    sol = solve_city(*borders)
+#                                     borders = [top, left, bottom, right]
+#                                     sol = solve_city(*borders)
                                     
-                                    if sol is None:
-                                        invalids.append(borders)
-                                    else:
-                                        is_valid = check_solution(*borders, sol)
-                                        if is_valid:
-                                            valids.append(borders)
-                                        else:
-                                            invalids.append(borders)
+#                                     if sol is None:
+#                                         invalids.append(borders)
+#                                     else:
+#                                         is_valid = check_solution(*borders, sol)
+#                                         if is_valid:
+#                                             valids.append(borders)
+#                                         else:
+#                                             invalids.append(borders)
 
-                except KeyError as e:
-                    invalids.append([top, bottom])
+#                 except KeyError as e:
+#                     invalids.append([top, bottom])
                     
-                    print("\nInvalid\n\n")
-                    input()
+#                     print("\nInvalid\n\n")
+#                     input()
 
-print(len(invalids))
-with open("./results/invalids.txt", "w") as f:
-    f.write("\n".join([str(i) for i in invalids]))
+# print(len(invalids))
+# with open("./results/invalids.txt", "w") as f:
+#     f.write("\n".join([str(i) for i in invalids]))
 
-with open("./results/unique-valids.txt", "w") as f:
-    f.write("\n".join([str(i) for i in valids]))
+# with open("./results/unique-valids.txt", "w") as f:
+#     f.write("\n".join([str(i) for i in valids]))
+
